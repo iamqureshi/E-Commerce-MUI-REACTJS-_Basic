48446278466744e6ff43a6249ffd5055bc288580
shaikh-smr <shaikhsamir.co.in@gmail.com>
2023-01-30 23:54:15 +0530

    install react-router-dom and update the readme.md file

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import './App.css';
+import "./App.css";
 import reportWebVitals from "./reportWebVitals";
 import theme from "./Styles/Theme";
 
